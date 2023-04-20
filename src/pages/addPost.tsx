52454f1421d074a8ea1a89b87d9d6d0ae8e34817
dongha.kim <dongha.kim@dongha.kim>
2023-04-20 22:45:29 +0900

    feat: post test in progress

---
+++ src/pages/addPost.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import Head from "next/head";
+
+import axios from "axios";
+
+import { useRouter } from "next/router";
+import NewPostForm from "@/components/NewPostForm";
+
+export default function Profile({ user }: any) {
+  const router = useRouter();
+
+  const handleSubmit = async ({ language, code }: any) => {
+    const { data } = await axios.post("/api/posts", {
+      language,
+      code,
+    });
+    console.log(data);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Create a Snippet</title>
+      </Head>
+      <div className="pt-8 pb-10 lg:pt-12 lg:pb-14 max-w-5xl mx-auto px-6 my-6">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-100 sm:text-5xl md:text-6xl mb-6">
+          Create a Snippet
+        </h1>
+
+        <div className="mt-6">
+          <NewPostForm className="max-w-5xl" onSubmit={handleSubmit} />
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/api/posts.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+function titleFromCode(code: string) {
+  return code.trim().split("\n")[0].replace("//", "");
+}
+
+export default async function handler(req: any, res: any) {
+  const { method } = req;
+  switch (method) {
+    case "POST":
+      const { language, code } = req.body;
+      const title = titleFromCode(code);
+      const post = await prisma.post.create({
+        data: {
+          title,
+          language,
+          code,
+        },
+      });
+      res.status(200).json(post);
+      break;
+    default:
+      res.setHeader("Allow", ["POST"]);
+      res.status(405).end("잘못된 호출입니다.");
+  }
+}
