8ddff1cc520285b72e9849e33be0ae6d07853d25
dongha1992 <kamdunga2002@gmail.com>
2023-04-24 00:33:18 +0900

    chore: user agent 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,12 +30,13 @@ export default function Home({ posts, userAgent }: any) {
     }
   };
   useEffect(() => {
-    console.log(window.navigator.userAgent);
+    const USER_AGENT =
+      "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19";
     if (typeof window !== undefined) {
       const navigatorClone = Object.assign({}, window.navigator);
       Object.defineProperties(navigatorClone, {
         userAgent: {
-          get: () => "ExampleApp/1.0 (iPhone)",
+          get: () => USER_AGENT,
         },
       });
       Object.defineProperty(window, "navigator", {
