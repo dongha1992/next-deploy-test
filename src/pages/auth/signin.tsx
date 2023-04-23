afdd6e0ee6a979d0dfc104b401a3ddf83e0b0f56
dongha1992 <kamdunga2002@gmail.com>
2023-04-23 23:54:35 +0900

    feat: user agen 우회

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,31 +1,15 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import { signIn, useSession, signOut } from "next-auth/react";
 import { getServerSession } from "next-auth/next";
 import { options } from "../api/auth/[...nextauth]";
 import Image from "next/image";
-import useIsMobile from "@/hooks/useIsMobile";
 
 function Signin() {
-  const isMobile = useIsMobile();
-  const originRef = useRef<any>(null);
-  const callbackUrlRef = useRef<any>(null);
-
-  useEffect(() => {
-    if (typeof window !== undefined) {
-      originRef.current = window.location.origin;
-      callbackUrlRef.current = `${window.location.origin}/auth/signin`;
-    }
-  }, []);
-
-  console.log(isMobile, "isMobile");
-
   return (
     <div className="h-full flex justify-center items-center">
-      <a
+      <button
         className="bg-white text-black p-4 rounded-sm flex"
         type="button"
-        // href={`http://${originRef.current}/api/auth/signin?callbackUrl=${callbackUrlRef.current}`}
-        target="_blank"
         onClick={() => signIn()}
       >
         <Image
@@ -35,7 +19,7 @@ function Signin() {
           height={25}
         />
         <span className="ml-4">start with google</span>
-      </a>
+      </button>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import router, { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
 import { signIn, useSession, signOut } from "next-auth/react";
 import { getServerSession } from "next-auth";
+import Head from "next/head";
 
 import { prisma } from "../../server/db/client";
 import { options } from "./api/auth/[...nextauth]";
@@ -10,7 +11,7 @@ import PostSmall from "@/components/PostSmall";
 import NewPostForm from "@/components/NewPostForm";
 import axios from "axios";
 
-export default function Home({ posts }: any) {
+export default function Home({ posts, userAgent }: any) {
   const { data, status } = useSession();
   const [isShowInput, setShowInput] = useState(false);
   const router = useRouter();
@@ -30,42 +31,54 @@ export default function Home({ posts }: any) {
   };
 
   return (
-    <div className="w-full pt-8 pb-10 mx-auto max-w-7xl px-10">
-      <div className="max-w-2xl mx-auto">
-        {!isShowInput && (
-          <input
-            className="relative w-full cursor-default rounded-xl border border-gray-300 bg-dark py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
-            placeholder="코드 작성하기"
-            onFocus={() => setShowInput(true)}
-          />
-        )}
+    <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `navigator.userAgent = "ExampleApp/1.0 (iPhone)"`,
+          }}
+        />
+      </Head>
+      <div className="w-full pt-8 pb-10 mx-auto max-w-7xl px-10">
+        <div className="max-w-2xl mx-auto">
+          {!isShowInput && (
+            <input
+              className="relative w-full cursor-default rounded-xl border border-gray-300 bg-dark py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
+              placeholder="코드 작성하기"
+              onFocus={() => setShowInput(true)}
+            />
+          )}
 
-        {isShowInput && (
-          <NewPostForm className="max-w-5xl" onSubmit={handleSubmit} />
-        )}
+          {isShowInput && (
+            <NewPostForm className="max-w-5xl" onSubmit={handleSubmit} />
+          )}
 
-        <ul className="mt-8">
-          {posts?.map((post: any) => (
-            <li key={post.id}>
-              <PostSmall
-                post={post}
-                href={`/post/${post.id}`}
-                user={post.user}
-                className="my-10"
-                onLike={() => console.log("like post", post.id)}
-                onComment={() => console.log("comment post", post.id)}
-                onShare={() => console.log("share post", post.id)}
-              />
-            </li>
-          ))}
-        </ul>
+          <ul className="mt-8">
+            {posts?.map((post: any) => (
+              <li key={post.id}>
+                <PostSmall
+                  post={post}
+                  href={`/post/${post.id}`}
+                  user={post.user}
+                  className="my-10"
+                  onLike={() => console.log("like post", post.id)}
+                  onComment={() => console.log("comment post", post.id)}
+                  onShare={() => console.log("share post", post.id)}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
 export async function getServerSideProps(context: any) {
   const session = await getServerSession(context.req, context.res, options);
+  const userAgent = context.req
+    ? context.req.headers["user-agent"] || ""
+    : navigator.userAgent;
 
   if (!session) {
     return {
@@ -87,6 +100,6 @@ export async function getServerSideProps(context: any) {
   });
 
   return {
-    props: { posts: JSON.parse(JSON.stringify(posts)) },
+    props: { posts: JSON.parse(JSON.stringify(posts)), userAgent },
   };
 }
