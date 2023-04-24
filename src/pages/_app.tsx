3118ba19e0f2961a4f5b336a9a6cc98ec961d564
dongha1992 <kamdunga2002@gmail.com>
2023-04-24 21:32:33 +0900

    feat: rq 설치

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { AppProps } from "next/app";
 import { SessionProvider, useSession } from "next-auth/react";
 import { NextPage } from "next";
-import { ReactElement, ReactNode } from "react";
+import { ReactElement, ReactNode, useRef } from "react";
 
 import "@/styles/globals.css";
 import "highlight.js/styles/stackoverflow-dark.css";
 import Layout from "@/components/Layout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -17,9 +18,22 @@ type AppPropsWithLayout = AppProps & {
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => <Layout>{page}</Layout>);
+  const queryClient = useRef<QueryClient>();
+
+  if (!queryClient.current) {
+    queryClient.current = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+        },
+      },
+    });
+  }
   return (
-    <SessionProvider session={pageProps.session}>
-      {getLayout(<Component {...pageProps} />)}
-    </SessionProvider>
+    <QueryClientProvider client={queryClient.current}>
+      <SessionProvider session={pageProps.session}>
+        {getLayout(<Component {...pageProps} />)}
+      </SessionProvider>
+    </QueryClientProvider>
   );
 }
