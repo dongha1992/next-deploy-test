78a833a674896900a896b3ac705f05ec6e083a6f
dongha1992 <kamdunga2002@gmail.com>
2023-04-25 21:00:15 +0900

    chore: test

--- src/_middleware.ts
+++ src/_middleware.ts
@@ -2,7 +2,7 @@ import { NextFetchEvent, NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-const secret = process.env.SECRET;
+const secret = process.env.NEXT_PUBLIC_JWT_SECRET_KEY;
 
 export async function middleware(req: NextRequest, event: NextFetchEvent) {
   const session = await getToken({ req, secret, raw: true });
