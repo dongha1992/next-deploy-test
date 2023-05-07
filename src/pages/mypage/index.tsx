41d75217a5f02f13a97a438fe417a73a1b5db0db
dongha1992 <kamdunga2002@gmail.com>
2023-05-08 01:26:53 +0900

    chore: css 수정

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import Navigation from "@/components/Common/Navigation";
+import Layout from "@/components/Layout";
+import React, { ReactElement } from "react";
 
 function Mypage() {
   return <div>개발중</div>;
 }
 
+Mypage.getLayout = (page: ReactElement) => {
+  return <Layout bottom={<Navigation />}>{page}</Layout>;
+};
 export default Mypage;
