950cf07383bfa95bb09172ac31c71dd83744f68e
dongha1992 <kamdunga2002@gmail.com>
2023-05-21 21:29:47 +0900

    feat: 인앱 브라우저 ing

--- src/hooks/useFormatUserAgent.ts
+++ src/hooks/useFormatUserAgent.ts
@@ -2,15 +2,15 @@ import { useEffect } from "react";
 
 function useFormatUserAgent() {
   const replaceUrl = () => {
-    if (navigator.userAgent.indexOf("KAKAO")) {
-      window.open(`${process.env.NEXTAUTH_URL}/book`);
-    } else if (navigator.userAgent.indexOf("Instagram")) {
-      window.open(`${process.env.NEXTAUTH_URL}/book`);
-    }
+    // if (navigator.userAgent.indexOf("KAKAO")) {
+    //   window.open(`${process.env.NEXTAUTH_URL}/book`);
+    // } else if (navigator.userAgent.indexOf("Instagram")) {
+    //   window.open(`${process.env.NEXTAUTH_URL}/book`);
+    // }
   };
 
   useEffect(() => {
-    replaceUrl();
+    // replaceUrl();
     // const USER_AGENT =
     //   "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19";
     // if (typeof window !== undefined) {
