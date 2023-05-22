4f3db33ef18d7218abb4be526f72a5f50d3934cf
dongha1992 <kamdunga2002@gmail.com>
2023-05-22 17:14:25 +0900

    feat: test

--- src/hooks/useFormatUserAgent.ts
+++ src/hooks/useFormatUserAgent.ts
@@ -1,17 +1,7 @@
 import { useEffect } from "react";
 
 function useFormatUserAgent() {
-  const replaceUrl = () => {
-    // if (navigator.userAgent.indexOf("KAKAO") > -1) {
-    //   window.open(`${process.env.NEXTAUTH_URL}/book`, "_self");
-    // } else if (navigator.userAgent.indexOf("Instagram") > -1) {
-    //   window.open(`${process.env.NEXTAUTH_URL}/book`, "_self");
-    // }
-    // return;
-  };
-
   useEffect(() => {
-    replaceUrl();
     // const USER_AGENT =
     //   "Mozilla/5.0 AppleWebKit/535.19 Chrome/56.0.0 Mobile Safari/535.19";
     // if (typeof window !== undefined) {
