901f351e2cf1b8b6579f9b13dcd43a2acc21a4bb
dongha1992 <kamdunga2002@gmail.com>
2023-05-27 20:03:46 +0900

    fix: bug

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 import Button from "./Common/Button";
 
-function LoginButton(onClick: any) {
+function LoginButton({ onClick }: any) {
   return (
     <Button
       className="flex bg-white text-black p-4 rounded-md text-md w-100 hover:bg-white focus:outline-none focus:ring-0"
