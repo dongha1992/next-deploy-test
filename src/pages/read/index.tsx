590f8580289a1426e749a232fdbda8af8c613bb2
dongha1992 <kamdunga2002@gmail.com>
2023-05-28 20:54:53 +0900

    chore: css 수정

--- src/pages/read/index.tsx
+++ src/pages/read/index.tsx
@@ -3,7 +3,11 @@ import Layout from "@/components/Layout";
 import React, { ReactElement } from "react";
 
 function ReadPage() {
-  return <div>ReadPage</div>;
+  return (
+    <div className="w-full pt-8 pb-14 mx-auto max-w-7xl px-4 bg-black relative">
+      개발 중!
+    </div>
+  );
 }
 
 ReadPage.getLayout = (page: ReactElement) => {
